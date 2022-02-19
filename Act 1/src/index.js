dd7c3e6a413627b5c6d3f33f3a20c81c6d213213
JMEmoria <jmanonymous090@gmail.com>
2022-02-19 13:34:53 +0800

    first commit

---
+++ Act 1/src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hi  Welcom to Vailla!</h1>
+<div>
+Vanilla DELIVERY!TN: 770507408271 Rider:Jhon Michael Emoria 639753629877.
+For COD pls prepare exact amount. Photo proof of delivery is required.
+Thanks and be safe!
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
